bc7e18b9b18578322d435666c13142278af22255
GBardis <crisolivasavatage@hotmail.com>
2017-01-23 22:25:46 +0200

    random changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,16 @@
 class UserMailer < ApplicationMailer
- default from: "mail@gmail.com"
+    default from: 'mail@gmail.com'
 
-  def contact_email(user)
-   @user = user
+    def contact_email(user)
+        @user = user
 
-   #RECIPIENT MAIL - STATIC
-   mail = 'georgebardis1990@gmail.com'
-    mail(to: mail, subject: 'Sample Email')
+        # RECIPIENT MAIL - STATIC
+        mail = 'mail@gmail.com@gmail.com'
+        mail(to: mail, subject: 'Sample Email')
+    end
 
-  end
-
-
-  def mailer(user)
-
-    @user = current_user
-    mail(to: @user.email, subject: 'Sample Email')
-  end
-
-
+    def mailer(_user)
+        @user = current_user
+        mail(to: @user.email, subject: 'Sample Email')
+    end
 end
