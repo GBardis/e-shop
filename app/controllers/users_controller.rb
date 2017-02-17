4450deaa96b6725713573fbbbc278a3d53fee1b9
GBardis <crisolivasavatage@hotmail.com>
2017-02-17 15:08:58 +0200

    Main nav completed and  some minor bug fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+  before_action :validate_authorization_for_user
   def show
     @user = User.find(params[:id])
   end
 
   def validate_authorization_for_user
-    redirect_to root_path unless @user == current_user
+    redirect_to root_path unless current_user
   end
 end
