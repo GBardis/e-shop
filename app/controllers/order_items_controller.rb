d8dc4291447bd28d7b3728a7647a7f3813f62615
GBardis <crisolivasavatage@hotmail.com>
2017-05-12 01:41:11 +0300

    at last cart increment works perfect :)

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -10,8 +10,10 @@ class OrderItemsController < ApplicationController
       # byebug
       @order.save
     else
-      byebug
+      @order_item = @order.order_items.find_by(product_id: @product.id)
       @order_item.update(quantity: @order_item.quantity + 1 )
+      @order_items = @order.order_items
+
     end
     session[:order_id] = @order.id
     respond_to do |format|
